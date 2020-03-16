c9d162c810cedbdc491557eb6256a783d298f7ce
nelsito <nelson.puglisi@gmail.com>
2020-03-16 19:49:48 -0300

    styles: * custom font Roboto * Empty action bar * default styles for progress, dialogs, etc. * bottom bar

--- app/src/main/kotlin/com/nelsito/travelplan/TripsActivity.kt
+++ app/src/main/kotlin/com/nelsito/travelplan/TripsActivity.kt
@@ -3,12 +3,12 @@ package com.nelsito.travelplan
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_trips.*
 
 class TripsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_trips)
 
         val user = FirebaseAuth.getInstance().currentUser
         txt_hello.text = "Hello ${user?.displayName}"
