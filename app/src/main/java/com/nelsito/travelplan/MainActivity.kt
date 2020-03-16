7f193f6cb77193f4596d74d081bb6328e8362883
nelsito <nelson.puglisi@gmail.com>
2020-03-16 12:41:11 -0300

    added firebase auth (email, google)

--- app/src/main/java/com/nelsito/travelplan/MainActivity.kt
+++ app/src/main/java/com/nelsito/travelplan/MainActivity.kt
@@ -2,11 +2,32 @@ package com.nelsito.travelplan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val RC_SIGN_IN = 4346
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openLogin()
+    }
+
+    private fun openLogin() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                //.setTheme(R.style.LoginAppTheme) // Set theme
+                .build(),
+            RC_SIGN_IN)
     }
 }
