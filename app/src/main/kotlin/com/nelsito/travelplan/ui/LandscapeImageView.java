16bc0876e609e18e4568864946462f6518c87e50
nelsito <nelson.puglisi@gmail.com>
2020-03-17 14:31:22 -0300

    trip_item_list layout

---
+++ app/src/main/kotlin/com/nelsito/travelplan/ui/LandscapeImageView.java
@@ -0,0 +1,31 @@
+package com.nelsito.travelplan.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class LandscapeImageView extends AppCompatImageView
+{
+    public LandscapeImageView(Context context)
+    {
+        super(context);
+    }
+
+    public LandscapeImageView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public LandscapeImageView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth() * 9 / 16); //Snap to width
+    }
+}
