a96216e28092e64e58a21cdde17cb2cb9a31937f
nelsito <nelson.puglisi@gmail.com>
2020-03-20 19:01:24 -0300

    remove temporal test

--- app/src/test/kotlin/com/nelsito/travelplan/domain/TripTest.kt
+++ app/src/test/kotlin/com/nelsito/travelplan/domain/TripTest.kt
@@ -14,7 +14,7 @@ class TripShould {
         //given
         val dateFrom = LocalDate.now(ZoneOffset.UTC).plusMonths(2).atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli()
         val dateTo = LocalDate.now(ZoneOffset.UTC).plusMonths(3).atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli()
-        val aFutureTrip = Trip("some id", "some place id", "some detination", "some description", dateFrom, dateTo)
+        val aFutureTrip = Trip( "some place id", "some detination", "some description", dateFrom, dateTo)
         //when
         val actual = aFutureTrip.daysToGo()
         //then
@@ -27,25 +27,11 @@ class TripShould {
         //given
         val dateFrom = LocalDate.now(ZoneOffset.UTC).minusMonths(2).atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli()
         val dateTo = LocalDate.now(ZoneOffset.UTC).minusMonths(1).atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli()
-        val aFutureTrip = Trip("some id", "some place id", "some detination", "some description", dateFrom, dateTo)
+        val aFutureTrip = Trip( "some place id", "some detination", "some description", dateFrom, dateTo)
         //when
         val actual = aFutureTrip.daysToGo()
         //then
         val expected = -60
         Assertions.assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `test formatter`() {
-        //given
-        val dateFrom = LocalDate.now(ZoneOffset.UTC).plusWeeks(1).plusMonths(2).atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli()
-        val format = "MMM dd, YYYY"
-
-        val from = LocalDateTime.from(org.threeten.bp.Instant.ofEpochMilli(dateFrom).atZone(ZoneOffset.UTC)).format(
-            DateTimeFormatter.ofPattern(format, Locale.getDefault()))
-        //when
-        //then
-        val expected = "MAY 25, 2020"
-        Assertions.assertThat(from).isEqualTo(expected)
     }
 }
