9191af244cf5dcb0c491c14454ccdbfa752861fe
nelsito <nelson.puglisi@gmail.com>
2020-03-20 21:17:42 -0300

    fix InMemoryRepo

--- app/src/main/kotlin/com/nelsito/travelplan/infra/InMemoryTripRepository.kt
+++ app/src/main/kotlin/com/nelsito/travelplan/infra/InMemoryTripRepository.kt
@@ -6,22 +6,28 @@ import com.nelsito.travelplan.domain.Trip
 import com.nelsito.travelplan.domain.TripRepository
 import kotlinx.coroutines.delay
 import java.util.*
+import kotlin.collections.HashMap
 
 class InMemoryTripRepository(private val repository: TripRepository) : TripRepository {
-    private val myTrips = hashMapOf<String, Trip>()
+    private val myTrips = hashMapOf<String, HashMap<String, Trip>>()
     override suspend fun add(user: FirebaseUser, trip: Trip): Boolean {
-        myTrips[trip.placeId] = trip
+        if (!myTrips.containsKey(user.uid)) {
+            myTrips[user.uid] = hashMapOf()
+        }
+        myTrips[user.uid]!![trip.placeId] = trip
         return repository.add(user, trip)
     }
 
     override suspend fun getTrips(user: FirebaseUser): List<Trip> {
-        if (myTrips.size == 0) {
+        if (!myTrips.containsKey(user.uid)) {
+            myTrips[user.uid] = hashMapOf()
+        }
+        if (myTrips[user.uid]?.size == 0) {
             repository.getTrips(user).forEach {
-                myTrips[it.placeId] = it
+                myTrips[user.uid]!![it.placeId] = it
             }
         }
-
-        return myTrips.values.toList()
+        return myTrips[user.uid]!!.values.toList()
     }
 
     override suspend fun searchTrips(user: FirebaseUser, search: Search): List<Trip> {
@@ -31,20 +37,20 @@ class InMemoryTripRepository(private val repository: TripRepository) : TripRepos
     }
 
     override suspend fun update(user: FirebaseUser, trip: Trip): Boolean {
-        myTrips[trip.placeId] = trip
+        myTrips[user.uid]!![trip.placeId] = trip
         return repository.update(user, trip)
     }
 
     override suspend fun find(user: FirebaseUser, placeId: String): Trip {
-        return if (myTrips.containsKey(placeId)) {
-            myTrips[placeId]!!
+        return if (myTrips.containsKey(user.uid) && myTrips[user.uid]!!.containsKey(placeId)) {
+            myTrips[user.uid]!![placeId]!!
         } else {
             repository.find(user, placeId)
         }
     }
 
     override suspend fun remove(user: FirebaseUser, trip: Trip): Boolean {
-        myTrips.remove(trip.placeId)
+        myTrips[user.uid]?.remove(trip.placeId)
         return repository.remove(user, trip)
     }
 }
