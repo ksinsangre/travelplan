c4cad01dcc963ccacc5f05fc67006fc0598fb3d8
nelsito <nelson.puglisi@gmail.com>
2021-04-18 19:02:52 -0300

    update api dependencies

--- api/functions/src/index.ts
+++ api/functions/src/index.ts
@@ -2,13 +2,12 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as express from 'express';
 import * as cors from 'cors';
-import * as bodyParser from 'body-parser';
 import { routesConfig } from './users/routes-config';
 
 admin.initializeApp();
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors({ origin: true }));
 routesConfig(app)
 
